62c9f41a7b5ae430ad762611071ce653fc08b1ed
Sakon <saadkondvilkar@gmail.com>
2021-03-31 11:15:43 +0530

    improved input statement

--- src/classes.py
+++ src/classes.py
@@ -1,42 +1,42 @@
 # Imports
 from sly import Lexer
 from sly import Parser
+import ast
 
 # Lexer Class Start
 class BasicLexer(Lexer):
 	tokens = { NAAM, NUMBER, STRING, PRINT, INPUT }
 	ignore = '\t '
-	literals = { '=', '+', '-', '/', 
-				'*', '(', ')', ',', ';'}
-  
-  
+	literals = { '=', '+', '-', '/', '*', '%'
+			'(', ')', ',', ';'}
+
 	# Define tokens as regular expressions
 	# (stored as raw strings)
 	NAAM = r'[a-zA-Z_][a-zA-Z0-9_]*'
 	STRING = r'\".*?\"'
 	NAAM["eww"] = PRINT
 	NAAM["input"] = INPUT
-  
+
 	# Number token
 	@_(r'\d+')
 	def NUMBER(self, t):
-		
 		# convert it into a python integer
 		t.value = int(t.value) 
 		return t
 
-	#remove quotes
-	def remove_quotes(self, text: str):
-		if text.startswith('\"') or text.startswith('\''):
-			return text[1:-1]
-		else:
-			pass
+	@_(r'\".*?\"')
+	def STRING(self, t):
+		# un-escape escaped characters
+		if len(t.value) > 0:
+			t.value = ast.literal_eval(t.value)
+
+		return t
 
 	# Comment token
 	@_(r'//.*')
 	def COMMENT(self, t):
 		pass
-  
+
 	# Newline token(used only for showing
 	# errors in new line)
 	@_(r'\n+')
@@ -72,14 +72,6 @@ class BasicParser(Parser):
 	def statement(self, p):
 		return p.var_assign
 
-# 	@_('NAAM "=" expr')
-# 	def var_assign(self, p):
-# 		return ('var_assign', p.NAAM, p.expr)
-
-# 	@_('NAAM "=" STRING')
-# 	def var_assign(self, p):
-# 		return ('var_assign', p.NAAM, p.STRING)
-
 	@_('NAAM "=" statement')
 	def var_assign(self, p):
 		return ('var_assign', p.NAAM, p.statement)
@@ -89,7 +81,7 @@ class BasicParser(Parser):
 		return (p.expr)
 
 	@_('STRING')
-	def statement(self, p):
+	def expr(self, p):
 		return (p.STRING)
 
 	@_('expr "+" expr')
@@ -125,9 +117,9 @@ class BasicParser(Parser):
 	def statement(self, p):
 		return ('print', p.statement)
 
-	@_('INPUT')
+	@_('INPUT statement')
 	def statement(self, p):
-		return ('input',)
+		return ('input', p.statement)
 
 # Parser Class End
 
@@ -138,34 +130,34 @@ class BasicExecute:
 		self.env = env
 		self.conf = config
 		result = self.walkTree(tree)
-		if isinstance(result, int) or isinstance(result, str):
-			print(result)
+		if result is not None and (isinstance(result, int) or isinstance(result, str)):
+			print(repr(result))
 
 	def walkTree(self, node):
 
 		if self.conf["DEBUG"] == True:
-			print(node)
+			print("[DEBUG]:", repr(node))
 
 		if isinstance(node, int) or isinstance(node, str):
 			return node
 
-		if node is None:
+		elif node is None:
 			return None
 
-		if node[0] == 'program':
+		elif node[0] == 'program':
 			if node[1] == None:
 				self.walkTree(node[2])
 			else:
 				self.walkTree(node[1])
 				self.walkTree(node[2])
 
-		if node[0] == 'num':
+		elif node[0] == 'num':
 			return node[1]
 
-		if node[0] == 'str':
-			return node[1]
+		elif node[0] == 'str':
+			return node[0]
 
-		if node[0] == 'add':
+		elif node[0] == 'add':
 			return self.walkTree(node[1]) + self.walkTree(node[2])
 		elif node[0] == 'sub':
 			return self.walkTree(node[1]) - self.walkTree(node[2])
@@ -174,14 +166,14 @@ class BasicExecute:
 		elif node[0] == 'div':
 			return self.walkTree(node[1]) / self.walkTree(node[2])
 		elif node[0] == 'print':
-			return self.walkTree(node[1])
+			print(self.walkTree(node[1]))
 		elif node[0] == 'input':
-			return f"\"{input()}\""
+			return input(self.walkTree(node[1]))
 
-		if node[0] == 'var_assign':
+		elif node[0] == 'var_assign':
 			self.env[node[1]] = self.walkTree(node[2])
 
-		if node[0] == 'var':
+		elif node[0] == 'var':
 			try:
 				return self.env[node[1]]
 			except LookupError:
